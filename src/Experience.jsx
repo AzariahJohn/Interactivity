ad28370962a4c87c6219499b03de15ba4c002064
Azariah John <azariah1200@gmail.com>
2023-03-11 13:50:04 +0530

    h

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,34 @@
-import { OrbitControls } from '@react-three/drei'
+import { FirstPersonControls, OrbitControls, PerspectiveCamera, PointerLockControls, useGLTF } from '@react-three/drei'
 import { Interactive } from '@react-three/xr'
-import { Perf } from 'r3f-perf'
 import React, { useState } from 'react'
-import {useGLTF} from '@react-three/drei'
 
 export default function Experience()
 {
-    const room = useGLTF("./lab.gltf")
+    const room = useGLTF("./lab``.gltf")
     const [cube, setCub] = useState()
 
     return <>
 
-        <OrbitControls makeDefault />
+        {/* <FirstPersonControls
+            movementSpeed={0.1}
+            lookSpeed={0.1}
+        ></FirstPersonControls> */}
+
+        <OrbitControls
+            target={[-1, 2, 0]}
+            minDistance={0.2}
+            maxDistance={1.2}
+            enableRotate
+            rotateSpeed={0.5}
+        />
+
+        <PerspectiveCamera makeDefault position={[3.7, 4.3, -1.5]}/>
+        {/* <mesh position={[-1, 2, 0]}>
+            <boxGeometry args={[0.1, 0.1, 0.1]}/>
+            <meshBasicMaterial color="grey"/>
+        </mesh> */}
+        
+        
 
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
--- src/index.jsx
+++ src/index.jsx
@@ -6,37 +6,16 @@ import { VRButton, XR, Controllers } from '@react-three/xr'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-function DotController({hand}) {
-    return(
-        <Controllers
-            hand={hand}
-        >
-
-        <mesh visible={false}/>
-        <mesh>
-            <sphereGeometry args={[0.01, 8, 8]} />
-            <meshBasicMaterial color="white" />
-        </mesh>
-
-        </Controllers>
-    )
-}
-
 function App(){
+
+    const radius = 0.002;
+
     return(
         <>
             <VRButton />
-            <Canvas
-            shadows
-            camera={{
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [ 0, 0, 0 ],
-            }}>
+            <Canvas shadows>
                 <XR>
-                    <DotController hand="left" />
-                    <DotController hand="right" />
+                    <Controllers />
                     <Experience />
                 </XR>
             </Canvas>
